9fc4b1c444b2292e0c16e0ecdd8a26718ce0c0cc
Teresa <teresajc2012@gmail.com>
2024-02-07 00:17:06 +0100

    inicio

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: terjimen <marvin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/06 20:15:28 by terjimen          #+#    #+#             */
+/*   Updated: 2024/02/06 21:54:31 by terjimen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include<unistd.h>
+# include<stdio.h>
+# include<stdlib.h>
+# include<fcntl.h>
+# include<limits.h>
+
+#endif
